2614a1aa5b281f260a22b8aa0b6913e093cde7d7
unknown <Administrator@LAB711.tumo.lab>
2014-07-09 19:37:39 +0400

    gallery

---
+++ wp-content/themes/blankslate/page-eskizas.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Ratatouille</title>
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src=js/bootstrap.js></script>
+        <link rel="icon" href="logo/favicon (1).ico" type="image/x-icon">
+		<link type="text/css" href="styles/bottom.css" rel="stylesheet" />
+		<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script type="text/javascript" src="lib/jquery.jcarousel.min.js"></script>
+		<script type="text/javascript" src="lib/jquery.pikachoose.min.js"></script>
+		<script type="text/javascript" src="lib/jquery.touchwipe.min.js"></script>
+		<script language="javascript">
+			$(document).ready(
+				function (){
+					$("#pikame").PikaChoose({carousel:true});
+				});
+		</script>
+	</head>
+	<body>
+    	
+		<header>
+        
+			<div class="logodiv">
+            <a  href="index.html">
+			<img src="logo/ratatouille-11.png"id="logo">
+            </a>
+			</div>
+			<div id="menudiv">
+			  
+			  
+			<ul class="menuul">
+            			<li class="menu">
+							<a href="cast.html">Cast</a></li>                   
+						<li class="menu">
+							<a href="behindthescenes.html">Behind the Scenes</a></li>
+                       	<li class="menu">
+							<a href="eskizas.html">Eskizas</a></li>
+						<li class="menu">
+							<a href="characters.html">Characters</a></li>
+                        <li class="menu">
+							<a href="gallery.html">Gallery</a></li>
+                       
+			</ul>
+			</div>
+			</div>
+		</header>
+		<div id="maindiv">
+		<div class="background">
+		<div class="pikachoose">
+		<ul id="pikame" class="jcarousel-skin-pika">
+		<li><img src="galleryeskiz/remy1.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy2.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy3.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy4.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy5.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy6.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy7.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy8.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/remy9.PNG" title="Remy"/></a></li>
+		<li><img src="galleryeskiz/gusteau1.PNG" title="Gusteau"/></a></li>
+		<li><img src="galleryeskiz/gusteau2.PNG" title="Gusteau"/></a></li>
+		<li><img src="galleryeskiz/gusteau3.PNG" title="Gusteau"/></a></li>
+		<li><img src="galleryeskiz/gusteau4.PNG" title="Gusteau"/></a></li>
+		<li><img src="galleryeskiz/gusteau5.PNG" title="Gusteau"/></a></li>
+		<li><img src="galleryeskiz/linguini1.PNG" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/linguini2.PNG" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/linguini3.PNG" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/linguini4.PNG" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/linguini5.PNG" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/linguini6.png" title="Linguini"/></a></li>
+		<li><img src="galleryeskiz/colette1.PNG" title="Colette"/></a></li>
+		<li><img src="galleryeskiz/colette2.PNG" title="Colette"/></a></li>
+		<li><img src="galleryeskiz/colette3.PNG" title="Colette"/></a></li>
+		<li><img src="galleryeskiz/colette4.PNG" title="Colette"/></a></li>
+		<li><img src="galleryeskiz/emile1.PNG" title="Emile"/></a></li>
+		<li><img src="galleryeskiz/emile2.PNG" title="Emile"/></a></li>
+		<li><img src="galleryeskiz/emile3.PNG" title="Emile"/></a></li>
+		<li><img src="galleryeskiz/ego1.PNG" title="Anton Ego"/></a></li>
+		<li><img src="galleryeskiz/ego2.PNG" title="Anton Ego"/></a></li>
+		<li><img src="galleryeskiz/skinner1.PNG" title="Skinner"/></a></li>
+		<li><img src="galleryeskiz/skinner2.PNG" title="Skinner"/></a></li>
+		<li><img src="galleryeskiz/django1.PNG" title="Django"/></a></li>
+		<li><img src="galleryeskiz/django2.PNG" title="Django"/></a></li>
+		<li><img src="galleryeskiz/django3.PNG" title="Django"/></a></li>
+		<li><img src="galleryeskiz/django4.PNG" title="Django"/></a></li>
+		<li><img src="galleryeskiz/chefs1.PNG" title="Chefs"/></a></li>
+		<li><img src="galleryeskiz/chefs2.PNG" title="Chefs"/></a></li>
+
+
+
+	</ul>
+</div>
+</div>
+</div>
+		
+		
+		<div class="footerdiv">
+			<div class="leftFooter">
+				<p class="textfooter11">Made by Arevik Arevyan.  </p>
+			</div>
+			
+			<div class="rightFooter">
+				
+				<div class="facebook">
+					<a  href="http://www.facebook.com" id="facebook" target="_blank"></a>
+				</div>
+				<div class="twitter">
+					 <a  href="http://www.twitter.com" id="twitter" target="_blank"></a>
+				</div>
+				<p class="find">  Find us on:</p>
+			</div>
+		</div>	
+		
+	</body>
+</html>
---
+++ wp-content/themes/blankslate/page-gallery.php
@@ -0,0 +1,111 @@
+<html>
+	<head>
+		<title>Ratatouille</title>
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src=js/bootstrap.js></script>
+        <link rel="icon" href="logo/favicon (1).ico" type="image/x-icon">
+		<link type="text/css" href="styles/bottom.css" rel="stylesheet" />
+		<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script type="text/javascript" src="lib/jquery.jcarousel.min.js"></script>
+		<script type="text/javascript" src="lib/jquery.pikachoose.min.js"></script>
+		<script type="text/javascript" src="lib/jquery.touchwipe.min.js"></script>
+		<script language="javascript">
+			$(document).ready(
+				function (){
+					$("#pikame").PikaChoose({carousel:true});
+				});
+		</script>
+	</head>
+	<body>
+    	
+		<header>
+        
+			<div class="logodiv">
+            <a  href="index.html">
+			<img src="logo/ratatouille-11.png"id="logo">
+            </a>
+			</div>
+			<div id="menudiv">
+			  
+			  
+			<ul class="menuul">
+            			<li class="menu">
+							<a href="cast.html">Cast</a></li>                   
+						<li class="menu">
+							<a href="behindthescenes.html">Behind the Scenes</a></li>
+                       	<li class="menu">
+							<a href="eskizas.html">Eskizas</a></li>
+						<li class="menu">
+							<a href="characters.html">Characters</a></li>
+                        <li class="menu">
+							<a href="gallery.html">Gallery</a></li>
+                       
+			</ul>
+			</div>
+			</div>
+		</header>
+		<div id="maindiv">
+		<div class="background">
+		<div class="pikachoose">
+		<ul id="pikame" class="jcarousel-skin-pika">
+		<li><img src="gallery/remy1.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy2.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy3.PNG" title="Remy"/></a></li>
+		<li><img src="gallery/remy4.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy5.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy6.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy7.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy8.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy9.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy10.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy11.png" title="Remy"/></a></li>
+		<li><img src="gallery/remy12.png" title="Remy"/></a></li>
+		<li><img src="gallery/gusteau1.PNG" title="Remy and Gusteau"/></a></li>
+		<li><img src="gallery/linguini1.PNG" title="Linguini"/></a></li>
+		<li><img src="gallery/linguini2.PNG" title="Linguini"/></a></li>
+		<li><img src="gallery/linguini3.PNG" title="Linguini"/></a></li>
+		<li><img src="gallery/colette1.PNG" title="Colette"/></a></li>
+		<li><img src="gallery/colette2.PNG" title="Colette"/></a></li>
+		<li><img src="gallery/colette3.PNG" title="Colette"/></a></li>
+		<li><img src="gallery/colette4.PNG" title="Colette"/></a></li>
+		<li><img src="gallery/emile1.png" title="Emile"/></a></li>
+		<li><img src="gallery/emile2.png" title="Emile"/></a></li>
+		<li><img src="gallery/emile3.PNG" title="Emile"/></a></li>
+		<li><img src="gallery/emile4.PNG" title="Emile"/></a></li>
+		<li><img src="gallery/emile5.PNG" title="Emile"/></a></li>
+		<li><img src="gallery/ego1.PNG" title="Anton Ego"/></a></li>
+		<li><img src="gallery/skinner1.PNG" title="Skinner"/></a></li>
+		<li><img src="gallery/skinner2.PNG" title="Skinner"/></a></li>
+		<li><img src="gallery/django1.PNG" title="Django"/></a></li>
+
+
+
+	</ul>
+</div>
+</div>
+</div>
+		
+		
+		
+		<div class="footerdiv">
+			<div class="leftFooter">
+				<p class="textfooter11">Made by Arevik Arevyan.  </p>
+			</div>
+			
+			<div class="rightFooter">
+				
+				<div class="facebook">
+					<a  href="http://www.facebook.com" id="facebook" target="_blank"></a>
+				</div>
+				<div class="twitter">
+					 <a  href="http://www.twitter.com" id="twitter" target="_blank"></a>
+				</div>
+				<p class="find">  Find us on:</p>
+			</div>
+		</div>
+		
+		
+		
+	</body>
+</html>
